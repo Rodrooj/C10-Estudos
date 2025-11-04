31b5098a103b3c3e35d690e1b4f19c03f21081ac
Rodrooj <rodrigobarbosapereira17@gmail.com>
2025-11-04 14:42:42 -0300

    Initial Commit

---
+++ C10-Estudos/C10_EstudosApp.swift
@@ -0,0 +1,17 @@
+//
+//  C10_EstudosApp.swift
+//  C10-Estudos
+//
+//  Created by Rodrigo Barbosa Pereira on 04/11/25.
+//
+
+import SwiftUI
+
+@main
+struct C10_EstudosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
